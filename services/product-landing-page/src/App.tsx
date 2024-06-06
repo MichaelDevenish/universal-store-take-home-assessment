cf677c12b0dfb7c16a12e98230d3d864a19b2615
lachlanpaulsen1 <lachlan.paulsen@universalstore.com.au>
2024-06-06 17:58:37 +1000

    feat(PLP): setup a simple react app for interfacing with other services

---
+++ services/product-landing-page/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://fulfil.universalstore.com/static/version1717065130/frontend/universalstore/universalstore/default/images/logo.svg" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
